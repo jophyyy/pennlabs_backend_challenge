f2d6684e77c1d38333bb51db49bd3fc55081a1e5
jophyyy <jollyjophy222@gmail.com>
2026-09-12 16:52:35 -0400

    fixed tags_id in club table and added secondary table for tags as mentioned in initial thoughts for table relationships

--- models.py
+++ models.py
@@ -1,12 +1,15 @@
 from app import db
 
 class Club(db.Model):
-    code = db.Column(db.String)
+    code = db.Column(db.String, primary_key= true)
     name = db.Column(db.String)
     description = db.Column(db.String)
-    tags_id = db.Column(db.String)
+    tags = db.relationship("Tags")
 
-
+class Tags(db.Model):
+    tag_id = db.Column(db.Integer, primary_key = true)
+    name = db.Column(db.String)
+    
 
 
 
